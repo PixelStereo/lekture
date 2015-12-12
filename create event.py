71e10722266345804ea05225bd41e9bb106390c4
reno- <contact@rubiano.fr>
2015-12-12 14:20:30 +0100

    rewrite Event class with properties, and create a Timepoint class

---
+++ create event.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+from time import sleep
+
+from lekture import lekture
+
+debug = True
+lekture.debug = True
+lekture.events.debug = True
+
+my_event = lekture.events.Event()
+"""print my_event.name
+print my_event.uid
+print my_event.protocol
+print my_event.description
+print my_event.output
+print my_event.content
+print my_event.content.keys()
+print"""
+my_event.timepoint(10)
+
+another_event = lekture.events.Event(content={'zob':22},name='lol')
+print '------'
+another_event.content = {'plouf' : 32}
+print another_event.content
+
+print
+print 'events listing'
+print '--------------'
+for event in lekture.events.Event.instances.keys():
+    print 'event named' , event.name , ':' , event
+    for timepoint in event.timepoints:
+        print '     ' ,  'timepoint' , timepoint.index , ':' , timepoint.content
--- lekture/lekture_events.py
+++ lekture/lekture_events.py
@@ -1,3 +1,4 @@
+import weakref
 import random
 import lekture_functions as lekture
 from OSC import OSCMessage
@@ -8,27 +9,8 @@ from time import sleep
 
 debug=True
 
-db = {'data':{}}
-objects = {}
 
-def new(name='',protocol='',uid='',description = '',output='',event_content=''):
-    """create an event"""
-    if event_content == '':
-        event_content={"0":{'/node/string':'a string','/node/integer':[random.randint(65e2,65e3)],'/node/float':[float(random.randint(0,100))/100],'/node/list':[float(random.randint(0,100))/100,random.randint(65e2, 65e3),"egg"]}}
-    if protocol == '':
-        protocol = 'OSC'
-    if output == '':
-        output = '127.0.0.1:10000'
-    if uid == '':
-        uid = lekture.timestamp()
-    if description == '':
-        description = "write a comment"
-    if name == '':
-        name = 'untitled'
-    if debug : print ("new event : " + uid)
-    event_new = Event(uid=uid,name=name,output=output,description=description,event_content=event_content)
-    objects.setdefault(uid,event_new)
-    return uid
+""" TODO : REWRITE PLAY - DELETE FUNCTIONS FOR TIMEPOINT AND EVENT CLASS"""
 
 def play(name=''):
     uids=[]
@@ -42,30 +24,196 @@ def play(name=''):
     for item in uids:
         item.play()
 
-def listing():
-    """make list of events (need to be done generic (make indexes from criteria)"""
-    listing = db['data'].keys()
-    return listing
+
+class Timepoint(object):
+    """docstring for Timepoint"""
+    def __new__(self,index,content={}):
+        _new = object.__new__(self)
+        if debug :
+            print
+            print "........... TIMEPOINT created ..........."
+        return _new
+
+    def __init__(self,index,content={}):
+        if content == {}:
+            content = {'/node/string':'a string','/node/integer':[random.randint(65e2,65e3)],'/node/float':[float(random.randint(0,100))/100],'/node/list':[float(random.randint(0,100))/100,random.randint(65e2, 65e3),"egg"]}
+        self.index = index
+        self.content = content
+        print 'TIMEPOINT INDEX' , self.index
+        print 'TIMEPOINT CONTENT' , self.content
+        print "........... TIMEPOINT %s inited ..........." %str(index)
+        print
+
+    # ----------- CONTENT -------------
+    @property
+    def content(self):
+        "Current content of the timepoint"
+        return self.__content
+
+    @content.setter
+    def content(self, content):
+        self.__content = content
+
+    @content.deleter
+    def content(self):
+        pass
 
 
-class Event:
+class Event(object):
     """Create a new event"""
-    def __init__(self,name,uid,description,event_content,output):
-        db['data'].setdefault(uid, {'attributes' : {'name' : name , 'description' : description , 'content' : event_content , 'output' : output}})
+    instances = weakref.WeakKeyDictionary()
+    def __new__(self,*args,**kwargs):
+        _new = object.__new__(self)
+        Event.instances[_new] = None
+        if debug :
+            print
+            print "........... EVENT created ..........."
+            print
+        return _new
+
+    def __init__(self,name='',protocol='',uid='',description = '',output='',content=''):
+        """create an event"""
+        timepoints = []
+        if protocol == '':
+            protocol = 'OSC'
+        if output == '':
+            output = '127.0.0.1:10000'
+        if uid == '':
+            uid = lekture.timestamp()
+        if description == '':
+            description = "write a comment"
+        if name == '':
+            name = 'untitled'
         self.name=name
         self.output=output
         self.description=description
-        self.content=event_content
+        self.protocol = protocol
         self.uid=uid
+        self.timepoints = timepoints
+        self.timepoint(0)
+        if content != '' : 
+            self.content=content
         if debug : 
             print
-            print '---------INIT EVENT' , self.name , '-------------'
-            print 'content : ' , self.content
+            print "........... EVENT %s initing ..........." %self.name
+            for timepoint in self.timepoints:
+                print 'timepoint' , timepoint.index , ':' , timepoint.content
             print 'name : ' , self.name
             print 'description : ' , self.description
             print 'output : ' , self.output
             print 'uid : ' , self.uid
+            print "........... EVENT %s inited ..........." %self.name
             print
+
+
+    def timepoint(self,index):
+        """create a timepoint"""
+        timepoint = Timepoint(index)
+        self.timepoints.append(timepoint)
+        return timepoint
+
+
+    # ----------- CONTENT -------------
+    @property
+    def content(self):
+        "Current content of the event (shortcut to timepoint 0) - TODO : CONCATENATE ALL TIMEPOINTS"
+        return self.timepoints[0].content
+
+    @content.setter
+    def content(self, content):
+        self.timepoints[0].content = content
+
+    @content.deleter
+    def content(self):
+        pass
+
+    # ----------- TIMEPOINTS -------------
+    @property
+    def timepoints(self):
+        "Current timepoints of the event"
+        return self.__timepoints
+
+    @timepoints.setter
+    def timepoints(self, timepoints):
+        self.__timepoints = timepoints
+
+    @timepoints.deleter
+    def timepoints(self):
+        pass
+
+    # ----------- NAME -------------
+    @property
+    def name(self):
+        "Current name of the event"
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @name.deleter
+    def name(self):
+        pass
+
+    # ----------- PROTOCOL -------------
+    @property
+    def protocol(self):
+        "Current protocol of the event"
+        return self.__protocol
+
+    @protocol.setter
+    def protocol(self, protocol):
+        self.__protocol = protocol
+
+    @protocol.deleter
+    def protocol(self):
+        pass
+
+    # ----------- UID -------------
+    @property
+    def uid(self):
+        "Current uid of the event"
+        return self.__uid
+
+    @uid.setter
+    def uid(self, uid):
+        self.__uid = uid
+
+    @uid.deleter
+    def uid(self):
+        pass
+
+    # ----------- DECRIPTION -------------
+    @property
+    def description(self):
+        "Current description of the event"
+        return self.__description
+
+    @description.setter
+    def description(self, description):
+        self.__description = description
+
+    @description.deleter
+    def description(self):
+        pass
+
+    # ----------- OUTPUT -------------
+    @property
+    def output(self):
+        "Current output of the event"
+        return self.__output
+
+    @output.setter
+    def output(self, output):
+        self.__output = output
+
+    @output.deleter
+    def output(self):
+        pass
+
+
+
+
 
     def delete(self):
         """delete an event"""
@@ -76,31 +224,6 @@ class Event:
         if debug : print (self.uid + " EDIT : " + attr + ' -> ' , value)
         del db['data'][self.uid]['attributes'][attr]
         db['data'][self.uid]['attributes'].setdefault(attr,value)
-
-    def timepoint_new(self):
-        index = self.timepoint_available()
-        db['data'][self.uid]['attributes']['content'].setdefault(index,{})
-        print 'NEW TIMEPOINT' , index
-        return index
-
-    def timepoint_available(self,index = '0'):
-        """create a timepoint in the selected event"""
-        timepoints = db['data'][self.uid]['attributes']['content'].keys()
-        #timepoints = sorted(timepoints)
-        if str(index) not in timepoints:
-            index = str(index)
-            return index
-        else:
-            index = int(index) + 1
-            return self.timepoint_available(index=index)
-
-    def timepoint_delete(self,timepoint):
-        """delete a timepoint in the selected event"""
-        del db['data'][self.uid]['attributes']['content'][timepoint]
-
-    def timepoints_listing(self):
-        """list all timepoints"""
-        return db['data'][self.uid]['attributes']['content'].keys()
 
     def play(self):
         """play an event"""
@@ -127,3 +250,4 @@ class Event:
         print '--------end event uid : ' + self.uid , '--------------'
         print
         return 'done'
+
