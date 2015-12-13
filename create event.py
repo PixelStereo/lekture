22d2dc2a45feda40b90e0278fae6426199cb7aa8
reno- <contact@rubiano.fr>
2015-12-13 23:57:48 +0100

    update exemple

--- create event.py
+++ create event.py
@@ -6,7 +6,7 @@ from lekture import lekture
 
 debug = True
 lekture.debug = True
-lekture.events.debug = True
+lekture.events.debug = False
 
 my_event = lekture.events.Event(name='toto-la-roulette')
 """print my_event.name
@@ -14,24 +14,28 @@ print my_event.uid
 print my_event.protocol
 print my_event.description
 print my_event.output
-print my_event.content
+print my_event.contentç
 print my_event.content.keys()
 print"""
-my_event.timepoint(10)
+my_event.timepoint(500)
 
 another_event = lekture.events.Event(content={'zob':22},name='lol')
-print '------'
-another_event.content = {'plouf' : 32}
+another_event.content = {'/plouf' : 32}
 
 """ content doesn't work when creating a timepoint… AHHHHHHHH"""
-another_event.timepoint(1000,content={'/my_string':['blah',2]})
+another_event.timepoint(500,content={'/my_string':['blah',2]})
 
-print another_event.content
+print lekture.events.Event.instances.keys()
+
+del my_event
+
+print lekture.events.Event.instances.keys()
 
 print
-print 'events listing'
-print '--------------'
-for event in lekture.events.Event.instances.keys():
-    print 'event named' , event.name , ':' , event
-    for timepoint in event.timepoints:
-        print '     ' ,  'timepoint' , timepoint.index , ':' , timepoint.content
+print
+if lekture.events.Event.instances.keys():
+	print 'PLAY ALL EVENTS'
+	print '--------------'
+	for event in lekture.events.Event.instances.keys():
+		print 'play event :' , event.name
+		event.play()
