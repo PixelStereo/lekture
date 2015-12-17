68c2981edfd0618520b51ebbb7a45aa6b4e9440c
reno- <contact@rubiano.fr>
2015-12-17 22:29:40 +0100

    add an exemple for settings

---
+++ settings.py
@@ -0,0 +1,17 @@
+from PyQt5.QtCore import QSettings, QPoint
+
+settings = QSettings('foo', 'foo')
+
+settings.setValue('int_value', 42)
+settings.setValue('point_value', QPoint(10, 12))
+
+# This will write the setting to the platform specific storage.
+del settings
+
+settings = QSettings('foo', 'foo')
+
+int_value = settings.value('int_value', type=int)
+print("int_value: %s" % repr(int_value))
+
+point_value = settings.value('point_value', type=QPoint)
+print("point_value: %s" % repr(point_value))
