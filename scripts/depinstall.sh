162c588e3245b6d9b9babf74ac22164811a8c904
reno- <contact@rubiano.fr>
2015-12-20 00:37:00 +0100

    install brew for both OS X and linux

--- scripts/depinstall.sh
+++ scripts/depinstall.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+if [ "x$TRAVIS_OS_NAME" = "xosx" ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+fi
 brew install PyQt5
 pip install PyOSC
 pip install pyinstaller
