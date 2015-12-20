8bd0c8ec5ed13c709607925925107de277428ebf
reno- <contact@rubiano.fr>
2015-12-20 10:57:19 +0100

    sudo for apt-get

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,19 +6,17 @@ cd ${HOME}
 case "$TRAVIS_OS_NAME" in
   linux)
   echo 'this is linux, so apt-get'
-  apt-get install PyQt5
+  sudo apt-get install PyQt5
   
    ;;
   osx)
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   echo 'this is osx, so brew'
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install PyQt5
   ;;
 esac
 cd -
 
-brew doctor
-
-brew install PyQt5
 pip install PyOSC
 pip install pyinstaller
 pyinstaller --windowed --icon=icon/lekture.icns lekture.py
