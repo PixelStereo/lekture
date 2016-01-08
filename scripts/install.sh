79c0a0fdf1be630634e23e45a6bde3134b0b46ef
reno- <contact@rubiano.fr>
2016-01-08 13:06:12 +0100

    try to fix Build

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,10 +8,19 @@ pyinstaller --windowed --icon=icon/lekture.icns -n Lekture_${TRAVIS_TAG} main.py
 case "$TRAVIS_OS_NAME" in
   linux)
     echo "START LINUX INSTALL"
-      wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
-      tar -xzf cmake-3.2.2-Linux-x86_64.tar.gz
-      mv cmake-3.2.2-Linux-x86_64 cmake
-      git clone https://github.com/avilleret/mingw-w64-build ${HOME}/mingw-w64-install
+    echo ''
+    echo ''
+    echo ''
+    echo ''
+    echo ''
+    echo ''
+    ls -lisah
+    echo ''
+    echo ''
+    echo ''
+    echo ''
+    echo ''
+    echo ''
     echo "END LINUX INSTALL"
    ;;
   osx)
