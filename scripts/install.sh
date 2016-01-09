130aa6c9fe6731ce587c3a073aef1af83a60f39e
reno- <contact@rubiano.fr>
2016-01-09 09:37:06 +0100

    fix build

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,23 +9,18 @@ pyinstaller --windowed --icon=icon/lekture.icns -n Lekture_${TRAVIS_TAG} main.py
 case "$TRAVIS_OS_NAME" in
   linux)
     echo "START LINUX INSTALL"
-    echo ''
-    echo ''
-    echo ''
-    echo ''
-    echo ''
-    echo ''
-    ls -lisah dist/Lekture_${TRAVIS_TAG}
-    echo ''
-    echo ''
-    echo ''
+    cp functions.py dist/lekture_${TRAVIS_TAG}
+    cp panels.py dist/lekture_${TRAVIS_TAG}
+    cp child.py dist/lekture_${TRAVIS_TAG}
+    cp functions.py dist/lekture_${TRAVIS_TAG}
+    wget https://github.com/PixelStereo/PyProjekt/archive/master.zip
+    ls
+    unzip master.zip
+    mv PyProjekt-master/pyprojekt dist/lekture_${TRAVIS_TAG}
+    cd dist
+    zip -r lekture_${TRAVIS_TAG}_ubuntu.zip lekture_${TRAVIS_TAG}
     sudo rm /home/travis/build.sh
-    ls /home/travis
     sudo echo "#!/bin/sh">/home/travis/build.sh
-    cat /home/travis/build.sh
-    echo ''
-    echo ''
-    echo ''
     echo "END LINUX INSTALL"
    ;;
   osx)
@@ -40,11 +35,9 @@ case "$TRAVIS_OS_NAME" in
     mv PyProjekt-master/pyprojekt dist/lekture_${TRAVIS_TAG}.app/Contents/MacOS/
     cd dist
     zip -r lekture_${TRAVIS_TAG}_OSX.zip lekture_${TRAVIS_TAG}.app
-    echo "END OSX INSTALL"
     sudo rm /Users/travis/build.sh
-    ls /Users/travis
     sudo echo "#!/bin/sh">/Users/travis/build.sh
-    cat /Users/travis/build.sh
+    echo "END OSX INSTALL"
   ;;
 esac
 cd ../
