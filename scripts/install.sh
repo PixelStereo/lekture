32f745938f9367db3c3ed1f38ebd56f5dce418a4
reno- <contact@rubiano.fr>
2016-01-09 18:44:13 +0100

    fix typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,37 +12,37 @@ case "$TRAVIS_OS_NAME" in
     echo "list APP -1"
     ls dist
     echo "list APP 0"
-    ls dist/lekture_${TRAVIS_TAG}/
-    cp functions.py dist/lekture_${TRAVIS_TAG}/
-    cp panels.py dist/lekture_${TRAVIS_TAG}/
-    cp child.py dist/lekture_${TRAVIS_TAG}/
-    cp functions.py dist/lekture_${TRAVIS_TAG}/
+    ls dist/Lekture_${TRAVIS_TAG}/
+    cp functions.py dist/Lekture_${TRAVIS_TAG}/
+    cp panels.py dist/Lekture_${TRAVIS_TAG}/
+    cp child.py dist/Lekture_${TRAVIS_TAG}/
+    cp functions.py dist/Lekture_${TRAVIS_TAG}/
     echo "list APP 1"
-    ls dist/lekture_${TRAVIS_TAG}/
+    ls dist/Lekture_${TRAVIS_TAG}/
     wget https://github.com/PixelStereo/PyProjekt/archive/master.zip
     unzip master.zip
-    mkdir dist/lekture_${TRAVIS_TAG}/pyprojekt
+    mkdir dist/Lekture_${TRAVIS_TAG}/pyprojekt
     echo "list APP 2"
-    ls dist/lekture_${TRAVIS_TAG}/
-    mv PyProjekt-master/pyprojekt/* dist/lekture_${TRAVIS_TAG}/pyprojekt/
+    ls dist/Lekture_${TRAVIS_TAG}/
+    mv PyProjekt-master/pyprojekt/* dist/Lekture_${TRAVIS_TAG}/pyprojekt/
     cd dist
-    zip -r lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip lekture_${TRAVIS_TAG}
+    zip -r Lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip Lekture_${TRAVIS_TAG}
     sudo rm /home/travis/build.sh
     sudo echo "#!/bin/sh">/home/travis/build.sh
     echo "END LINUX INSTALL"
    ;;
   osx)
     echo "START OSX INSTALL"
-    cp functions.py dist/lekture_${TRAVIS_TAG}.app/Contents/MacOS/
-    cp panels.py dist/lekture_${TRAVIS_TAG}.app/Contents/MacOS/
-    cp child.py dist/lekture_${TRAVIS_TAG}.app/Contents/MacOS/
-    cp functions.py dist/lekture_${TRAVIS_TAG}.app/Contents/MacOS/
+    cp functions.py dist/Lekture_${TRAVIS_TAG}.app/Contents/MacOS/
+    cp panels.py dist/Lekture_${TRAVIS_TAG}.app/Contents/MacOS/
+    cp child.py dist/Lekture_${TRAVIS_TAG}.app/Contents/MacOS/
+    cp functions.py dist/Lekture_${TRAVIS_TAG}.app/Contents/MacOS/
     wget https://github.com/PixelStereo/PyProjekt/archive/master.zip
     ls
     unzip master.zip
     mv PyProjekt-master/pyprojekt dist/lekture_${TRAVIS_TAG}.app/Contents/MacOS/
     cd dist
-    zip -r lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip lekture_${TRAVIS_TAG}.app
+    zip -r Lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip Lekture_${TRAVIS_TAG}.app
     sudo rm /Users/travis/build.sh
     sudo echo "#!/bin/sh">/Users/travis/build.sh
     echo "END OSX INSTALL"
