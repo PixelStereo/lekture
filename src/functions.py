1b21190054fa0c4764dc888913c24fae3f46ccd7
reno- <contact@rubiano.fr>
2016-01-16 15:38:28 +0100

    clean

--- src/functions.py
+++ src/functions.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyprojekt import project
+import os, sys
+# for development of pyprojekt, use git version
+projekt_path = os.path.abspath('./../../PyProjekt')
+sys.path.append(projekt_path)
+
+from pyprojekt import functions
 
 def event2line(line):
     if isinstance(line,unicode):
-        line = project.unicode2string_list(line)
+        line = functions.unicode2string_list(line)
     if isinstance(line,int):
         line = str(line)
     else:
@@ -17,7 +22,7 @@ def line2event(self,newline):
     if isinstance(newline, unicode):
         newline = newline.encode('utf-8')
     newline = newline.split(' ')
-    newline = project.unicode2_list(newline)
+    newline = unicode2_list(newline)
     if isinstance(newline,float):
         newline = int(newline)
         self.scenario_content.currentItem().setText(str(newline))
@@ -28,3 +33,16 @@ def line2event(self,newline):
     elif len(newline) > 1:
         newline = [newline[0],newline[1:]]
     return newline
+
+def unicode2_list(data):
+    """convert unicode to list"""
+    if isinstance(data,list):
+        if len(data) == 1:
+            rv = data[0]
+            rv = functions.checkType(rv)
+        else:
+            rv = []
+            for item in data:
+                item = functions.checkType(item)
+                rv.append(item)
+    return rv
