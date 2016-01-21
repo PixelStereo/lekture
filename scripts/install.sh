0164a128e1eafb1000d09152c13bcdaf9ed8ac35
Renaud <contact@rubiano.fr>
2016-01-21 09:21:01 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,21 +4,21 @@ set -v
 
 cd src
 pyinstaller --onefile --paths ../3rdparty/PyProjekt/ --windowed --icon=icon/lekture.icns -n Lekture_${TRAVIS_TAG} main.py
-cd dist
-zip -r Lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip Lekture_${TRAVIS_TAG}.app
+cd distance
 
 case "$TRAVIS_OS_NAME" in
   linux)
     echo "START LINUX INSTALL"
+    zip -r Lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip Lekture_${TRAVIS_TAG}
     sudo rm /home/travis/build.sh
     sudo echo "#!/bin/sh">/home/travis/build.sh
     echo "END LINUX INSTALL"
    ;;
   osx)
     echo "START OSX INSTALL"
+    zip -r Lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip Lekture_${TRAVIS_TAG}.app
     sudo rm /Users/travis/build.sh
     sudo echo "#!/bin/sh">/Users/travis/build.sh
     echo "END OSX INSTALL"
   ;;
 esac
-
