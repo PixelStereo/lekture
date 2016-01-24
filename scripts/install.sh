3e308c7a0cdab298cba5a9bfab6daf21258f3418
reno- <contact@rubiano.fr>
2016-01-24 18:17:02 +0100

    useless

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,19 +8,9 @@ cd dist
 
 case "$TRAVIS_OS_NAME" in
   linux)
-    echo "START LINUX INSTALL"
-    ls -lisah
     zip Lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip Lekture_${TRAVIS_TAG}
-    sudo rm /home/travis/build.sh
-    sudo echo "#!/bin/sh">/home/travis/build.sh
-    echo "END LINUX INSTALL"
    ;;
   osx)
-    echo "START OSX INSTALL"
     zip -r Lekture_${TRAVIS_TAG}_$TRAVIS_OS_NAME.zip Lekture_${TRAVIS_TAG}.app
-    sudo rm /Users/travis/build.sh
-    sudo echo "#!/bin/sh">/Users/travis/build.sh
-    echo "END OSX INSTALL"
   ;;
 esac
-ls -lisah
