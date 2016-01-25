3de618aaf6b4285da60a183e4ac46de4f6e4d12b
reno- <contact@rubiano.fr>
2016-01-25 16:34:17 +0100

    fix python3 build

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,13 @@ set -v
 
 brew install PyQt5 --with-python --with-python3
 sudo pip install Cython
+sudo pip3 install Cython
 sudo pip install pyliblo
+sudo pip3 install pyliblo
 sudo pip install pyinstaller
+sudo pip3 install pyinstaller
+sudo pip install -r 3rdparty/pydular/requirements.txt
+sudo pip3 install -r 3rdparty/pydular/requirements.txt
 
 cd src
 ../scripts/build.sh ${TRAVIS_TAG}
