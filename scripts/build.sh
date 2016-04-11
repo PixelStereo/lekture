ccd3a2dfe9d4a29c41817ab7e9c1694118bd3665
reno- <contact@rubiano.fr>
2016-04-12 00:13:00 +0200

    FIX build on local (install deps

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,7 @@ echo "app will be build with the name :" $NAME
 
 # install pylekture dependancies
 sudo pip3 install -r ../3rdparty/pylekture/requirements.txt
+sudo pip3 install pyinstaller
 
 # build the binary
 pyinstaller --onefile --paths ../3rdparty/pylekture/ --windowed --icon=icon/icon.icns -n $NAME main.py
